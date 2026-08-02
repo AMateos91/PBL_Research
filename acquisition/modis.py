eec5c6bceaf2b965f6dcb315dcb713128de5ca30
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-03 00:02:12 +0200

    Add MODIS class for satellite data handling

---
+++ acquisition/modis.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from .satellite import Satellite
+
+
+class MODIS(Satellite):
+
+    SHORT_NAME = "MOD09GA"
+
+    @staticmethod
+    def bands():
+
+        return {
+
+            "red": "sur_refl_b01",
+
+            "nir": "sur_refl_b02",
+
+            "blue": "sur_refl_b03",
+
+            "green": "sur_refl_b04",
+
+            "swir1": "sur_refl_b06",
+
+            "swir2": "sur_refl_b07",
+
+        }
+
+    @staticmethod
+    def qa_band():
+
+        return "state_1km"
