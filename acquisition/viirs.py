1a898ffdcb2c327e8797fd440e9e4ed5ae91a770
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-03 00:07:13 +0200

    Add VIIRS class for satellite data handling

---
+++ acquisition/viirs.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from .satellite import Satellite
+
+
+class VIIRS(Satellite):
+
+    SHORT_NAME = "VNP09GA"
+
+    @staticmethod
+    def bands():
+
+        return {
+
+            "blue": "M3",
+
+            "green": "M4",
+
+            "red": "M5",
+
+            "nir": "M7",
+
+            "swir1": "M10",
+
+            "swir2": "M11",
+
+        }
+
+    @staticmethod
+    def qa_band():
+
+        return "QF2"
