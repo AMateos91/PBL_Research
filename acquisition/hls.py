73f6a3f22480555e112cea0b58a536e3a48fd2cc
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-02 23:55:56 +0200

    Refactor HLS class inheritance and methods
    Refactor HLS class to inherit from Satellite instead of EarthData, removing unnecessary methods.

--- acquisition/hls.py
+++ acquisition/hls.py
@@ -1,66 +1,11 @@
 from __future__ import annotations
 
-from pathlib import Path
-
-from .earthdata import EarthData
-
-from ..utils.constants import Dataset
-from ..utils.types import BoundingBox
-from ..utils.types import TimeRange
+from .satellite import Satellite
 
 
-class HLS(EarthData):
+class HLS(Satellite):
 
-    def __init__(self):
-
-        super().__init__()
-
-    def search(
-        self,
-        temporal: TimeRange,
-        bounding_box: BoundingBox,
-        cloud_cover: float | None = None,
-    ):
-
-        kwargs = {
-
-            "short_name": Dataset.HLSL30.value,
-
-            "temporal": temporal,
-
-            "bounding_box": bounding_box,
-
-        }
-
-        if cloud_cover is not None:
-
-            kwargs["cloud_cover"] = (
-                0,
-                cloud_cover,
-            )
-
-        return super().search(
-            **kwargs
-        )
-
-    def download(
-        self,
-        temporal: TimeRange,
-        bounding_box: BoundingBox,
-        directory: Path,
-        cloud_cover: float | None = None,
-    ):
-
-        results = self.search(
-            temporal=temporal,
-            bounding_box=bounding_box,
-            cloud_cover=cloud_cover,
-        )
-
-        return super().download(
-            results,
-            directory,
-        )
+    SHORT_NAME = "HLSL30"
 
     @staticmethod
     def bands():
