d2d707c63e495dd82aea0f22233614bf38cf509a
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-03 22:54:31 +0200

    Add additional regression models to init file

--- ml/classical/_init_.py
+++ ml/classical/_init_.py
@@ -1,12 +1,21 @@
 from .base import ClassicalModel
+
 from .linear import LinearRegressionModel
 from .ridge import RidgeRegressionModel
 from .lasso import LassoRegressionModel
 from .elasticnet import ElasticNetRegressionModel
+
 from .svm import SupportVectorRegressionModel
 from .knn import KNearestNeighborsRegressionModel
+
 from .decision_tree import DecisionTreeRegressionModel
 from .random_forest import RandomForestRegressionModel
+from .extra_trees import ExtraTreesRegressionModel
+from .gradient_boosting import GradientBoostingRegressionModel
+
+from .xgboost import XGBoostRegressionModel
+from .lightgbm import LightGBMRegressionModel
+from .catboost import CatBoostRegressionModel
 
 __all__ = [
     "ClassicalModel",
@@ -18,4 +27,9 @@ __all__ = [
     "KNearestNeighborsRegressionModel",
     "DecisionTreeRegressionModel",
     "RandomForestRegressionModel",
+    "ExtraTreesRegressionModel",
+    "GradientBoostingRegressionModel",
+    "XGBoostRegressionModel",
+    "LightGBMRegressionModel",
+    "CatBoostRegressionModel",
 ]
