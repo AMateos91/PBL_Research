0361a0779e2919903802d6d12944284b164cb10a
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-03 17:19:10 +0200

    Add ACTIVATEMask class for dataset processing

---
+++ preprocessing/masks/activate.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import xarray as xr
+
+from .base import Mask
+
+
+class ACTIVATEMask(Mask):
+
+    def apply(
+        self,
+        dataset: xr.Dataset,
+    ) -> xr.Dataset:
+
+        return dataset
