4a6fa7d5107401ecf805bf39a3d06d01d9029885
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-03 23:32:20 +0200

    Add new neural network models to init file

--- ml/neural/_init_.py
+++ ml/neural/_init_.py
@@ -2,9 +2,19 @@ from .base import NeuralModel
 
 from .mlp import MLPModel
 from .cnn import CNNModel
+from .lstm import LSTMModel
+from .gru import GRUModel
+from .transformer import TransformerModel
+from .autoencoder import AutoencoderModel
+from .unet import UNetModel
 
 __all__ = [
     "NeuralModel",
     "MLPModel",
     "CNNModel",
+    "LSTMModel",
+    "GRUModel",
+    "TransformerModel",
+    "AutoencoderModel",
+    "UNetModel",
 ]
