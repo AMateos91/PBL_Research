2b6126c50aa0b0ca2b923bfc0d0cc19f0080c30a
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-03 22:39:04 +0200

    Add ClassicalModel to module exports

--- ml/classical/_init_.py
+++ ml/classical/_init_.py
@@ -1,5 +1,13 @@
+from .base import ClassicalModel
 from .linear import LinearRegressionModel
+from .ridge import RidgeRegressionModel
+from .lasso import LassoRegressionModel
+from .elasticnet import ElasticNetRegressionModel
 
 __all__ = [
+    "ClassicalModel",
     "LinearRegressionModel",
+    "RidgeRegressionModel",
+    "LassoRegressionModel",
+    "ElasticNetRegressionModel",
 ]
