5d8cd0130bc3209853705054b03ffdf113c8ab84
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-03 17:00:24 +0200

    Add GEDIQuality class for quality masking

---
+++ preprocessing/qa/gedi.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import xarray as xr
+
+from .base import QualityMask
+
+
+class GEDIQuality(QualityMask):
+
+    def apply(
+        self,
+        dataset: xr.Dataset,
+    ) -> xr.Dataset:
+
+        return dataset
