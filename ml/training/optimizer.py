2ced33cd4ee72c3e59e0922c4358e6716e32cebc
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-04 14:37:25 +0200

    Implement Optimizers class for various optimizers

---
+++ ml/training/optimizer.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import torch.optim as optim
+
+from ...utils.constants import Optimizer
+
+
+class Optimizers:
+
+    _OPTIMIZERS = {
+
+        Optimizer.SGD: optim.SGD,
+
+        Optimizer.ADAM: optim.Adam,
+
+        Optimizer.ADAMW: optim.AdamW,
+
+        Optimizer.RMSPROP: optim.RMSprop,
+
+    }
+
+    @classmethod
+    def get(
+        cls,
+        optimizer: Optimizer,
+        parameters,
+        **kwargs,
+    ) -> optim.Optimizer:
+
+        if optimizer not in cls._OPTIMIZERS:
+
+            raise ValueError(
+                f"Unknown optimizer: {optimizer}"
+            )
+
+        return cls._OPTIMIZERS[
+            optimizer
+        ](
+            parameters,
+            **kwargs,
+        )
