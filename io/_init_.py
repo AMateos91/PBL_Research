c0bd1eaecb1c023b51447bc5fa758bf38013ad6d
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-04 18:20:21 +0200

    Initialize io module with reader imports

---
+++ io/_init_.py
@@ -0,0 +1,13 @@
+from .reader import Reader
+from .raster import RasterReader
+from .vector import VectorReader
+from .table import TableReader
+from .model import ModelReader
+
+__all__ = [
+    "Reader",
+    "RasterReader",
+    "VectorReader",
+    "TableReader",
+    "ModelReader",
+]
