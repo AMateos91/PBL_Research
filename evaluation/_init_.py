116eb65f0ecb8fcdde11bc4ac2693c3b65e0a396
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-04 16:37:17 +0200

    Initialize evaluation module with imports and exports

---
+++ evaluation/_init_.py
@@ -0,0 +1,11 @@
+from .evaluator import Evaluator
+from .regression import RegressionEvaluator
+from .classification import ClassificationEvaluator
+from .validation import Validator
+
+__all__ = [
+    "Evaluator",
+    "RegressionEvaluator",
+    "ClassificationEvaluator",
+    "Validator",
+]
