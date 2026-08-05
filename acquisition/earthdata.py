b0902f0508e062839e4391d4e0b871585c7a53b2
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-05 13:36:24 +0200

    Implement download method in EarthData class
    Added download method to EarthData class for downloading results.

--- acquisition/earthdata.py
+++ acquisition/earthdata.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 import earthaccess
 
 from ..utils.exceptions import AuthenticationError
 from ..utils.logger import Logger
 from .base import Acquisition
+from .downloader import Downloader
 
 
 class EarthData(Acquisition):
@@ -12,7 +15,6 @@ class EarthData(Acquisition):
     def __init__(self):
 
         self.logger = Logger.get(__name__)
-
         self._authenticated = False
 
 
@@ -32,6 +34,10 @@ class EarthData(Acquisition):
 
             self._authenticated = True
 
+            self.logger.info(
+                "Earthdata authentication successful."
+            )
+
         except Exception as exc:
 
             raise AuthenticationError(
@@ -49,4 +55,16 @@ class EarthData(Acquisition):
 
         return earthaccess.search_data(
             **kwargs
+        )
+
+
+    def download(
+        self,
+        results,
+        directory: Path,
+    ) -> list[Path]:
+
+        return Downloader().download(
+            results,
+            directory,
         )
