bd5c1e78e81f80d52d81f1d5f456de921deaf073
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-06 23:20:40 +0200

    Initialize collocation module with imports

---
+++ collocation/__init__.py
@@ -0,0 +1,9 @@
+from .collocator import Collocator
+from .spatial import SpatialCollocator
+from .temporal import TemporalCollocator
+
+__all__ = [
+    "Collocator",
+    "SpatialCollocator",
+    "TemporalCollocator",
+]
