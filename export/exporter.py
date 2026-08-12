a3eaaa22568405e95e04fa67730983fd741849a6
AMateos91 <70436917+AMateos91@users.noreply.github.com>
2026-08-12 19:36:29 +0200

    Add FigureExporter class for exporting figures

--- export/exporter.py
+++ export/exporter.py
@@ -12,3 +12,19 @@ class Exporter(ABC):
         **kwargs,
     ) -> None:
         ...
+
+class FigureExporter(Exporter):
+
+    def export(
+        self,
+        figure,
+        path: str,
+        dpi: int = 300,
+        bbox_inches: str = "tight",
+    ) -> None:
+        figure.canvas.draw()
+        figure.savefig(
+            path,
+            dpi=dpi,
+            bbox_inches=bbox_inches,
+        )
